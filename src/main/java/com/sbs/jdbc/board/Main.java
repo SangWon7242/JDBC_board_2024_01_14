2801923532228033b42e4110fbc6c96c34434a25
SangWon7242 <gotkdqja@gmail.com>
2024-01-20 09:43:06 +0900

    JDBC 연결 테스트

--- src/main/java/com/sbs/jdbc/board/Main.java
+++ src/main/java/com/sbs/jdbc/board/Main.java
@@ -1,9 +1,5 @@
 package com.sbs.jdbc.board;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
   public static void main(String[] args) {
     new App().run();
